c684020a7cd04d9053ad2c6d7f5c8cada3de001f
qxdFox <jusi.p@icloud.com>
2025-12-14 15:38:16 +0100

    fix scrollbar size

--- src/game/client/components/entity/menus_entity.cpp
+++ src/game/client/components/entity/menus_entity.cpp
@@ -3186,7 +3186,7 @@ void CMenus::RenderSettingsVisual(CUIRect MainView)
 	{
 		static float Offset = 0.0f;
 		Miscellaneous.VMargin(5.0f, &Miscellaneous);
-		Miscellaneous.HSplitTop(145.0f + Offset, &Miscellaneous, &DiscordRpc);
+		Miscellaneous.HSplitTop(135.0f + Offset, &Miscellaneous, &DiscordRpc);
 		if(s_ScrollRegion.AddRect(Miscellaneous))
 		{
 			Offset = 0.0f;
@@ -3251,7 +3251,7 @@ void CMenus::RenderSettingsVisual(CUIRect MainView)
 				DoButton_CheckBoxAutoVMarginAndSet(&g_Config.m_ClFreezeStars, Localize("Freeze stars"), &g_Config.m_ClFreezeStars, &Miscellaneous, LineSize);
 				DoButton_CheckBoxAutoVMarginAndSet(&g_Config.m_TcFreezeKatana, Localize("Show katana on frozen players"), &g_Config.m_TcFreezeKatana, &Miscellaneous, LineSize);
 
-				Miscellaneous.HSplitTop(25.f, &Button, &Miscellaneous);
+				Miscellaneous.HSplitTop(LineSize, &Button, &Miscellaneous);
 				Ui()->DoScrollbarOption(&g_Config.m_ClRenderCursorSpec, &g_Config.m_ClRenderCursorSpec, &Button, Localize("Cursor Opacity in Spec"), 0, 100, &CUi::ms_LinearScrollbarScale, 0u, "");
 			}
 		}
