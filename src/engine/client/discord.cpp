bc9df7d70e340eff4713a1ceaecc7186b3c96312
Ametic <delciakx@gmail.com>
2026-04-14 14:49:31 +0200

    Server name in Discord RPC

--- src/engine/client/discord.cpp
+++ src/engine/client/discord.cpp
@@ -3,6 +3,7 @@
 
 #include <engine/client.h>
 #include <engine/discord.h>
+#include <engine/shared/config.h>
 
 #if defined(CONF_DISCORD)
 #include <discord_game_sdk.h>
@@ -146,7 +147,10 @@ public:
 		m_Activity.timestamps.start = m_TimeStamp;
 		if(m_ShowMap)
 			str_copy(m_Activity.state, ServerInfo.m_aMap, sizeof(m_Activity.state));
-		str_copy(m_Activity.details, pDetail, sizeof(m_Activity.details));
+		if(g_Config.m_AcDiscordShowServerName)
+			str_copy(m_Activity.details, ServerInfo.m_aName, sizeof(m_Activity.details));
+		else
+			str_copy(m_Activity.details, pDetail, sizeof(m_Activity.details));
 
 		m_Activity.party.size.current_size = ServerInfo.m_NumClients;
 		m_Activity.party.size.max_size = ServerInfo.m_MaxClients;
@@ -174,7 +178,10 @@ public:
 
 		UpdateServerIp(ServerInfo);
 
-		str_copy(m_Activity.details, ServerInfo.m_aName, sizeof(m_Activity.details));
+		if(g_Config.m_AcDiscordShowServerName)
+			str_copy(m_Activity.details, ServerInfo.m_aName, sizeof(m_Activity.details));
+		else
+			str_copy(m_Activity.details, pDetail, sizeof(m_Activity.details));
 		if(m_ShowMap)
 			str_copy(m_Activity.state, ServerInfo.m_aMap, sizeof(m_Activity.state));
 		str_copy(m_Activity.details, pDetail, sizeof(m_Activity.details));
--- src/engine/shared/config_variables_axis.h
+++ src/engine/shared/config_variables_axis.h
@@ -11,3 +11,4 @@
 MACRO_CONFIG_INT(ClAClientSettingsTabs, ac_aclient_settings_tabs, 0, 0, 65536, CFGFLAG_CLIENT | CFGFLAG_SAVE, "Bit flags to disable settings tabs")
 MACRO_CONFIG_INT(ClAClientShopAutoSet, ac_aclient_shop_auto_set, 1, 0, 1, CFGFLAG_CLIENT | CFGFLAG_SAVE, "Apply downloaded shop assets automatically")
 MACRO_CONFIG_INT(AcNameplateSkins, ac_nameplate_skins, 0, 0, 1, CFGFLAG_CLIENT | CFGFLAG_SAVE, "Shows skin names in nameplates, good for finding missing skins")
+MACRO_CONFIG_INT(AcDiscordShowServerName, ac_discord_show_server_name, 1, 0, 1, CFGFLAG_CLIENT | CFGFLAG_SAVE, "Show server name in Discord status instead of game status (0 = game status, 1 = server name)")
